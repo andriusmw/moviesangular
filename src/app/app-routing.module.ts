b4f12ff255ab9779865afd5f49bcae7f7d140f56
andriusmw <andriusmw@yahoo.es>
2022-04-06 16:21:11 +0200

    enrutamiento básicado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
 import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [{ path: '', component: SearchComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'search' },
+  { path: '**', pathMatch: 'full', redirectTo: 'search' },
+  //Cuando busca algo que no está en las rutas redireciona a search
+  { path: 'search', component: SearchComponent },
+  { path: 'details', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
